592ad648ea08a7dd10c9c288647be83525370957
brunafinSD <79814692+brunafinSD@users.noreply.github.com>
2021-03-09 15:25:52 -0300

    Delete Semana 2/Desafio directory
    deletando pasta de projeto desafio para criar outra da forma certa. Desconsiderar todos esses arquivos.

--- Semana 2/Desafio/api-produtos/api/index.js
+++
@@ -1,8 +0,0 @@
-const config = require('config')
-const express = require('express')
-const app = express()
-
-const bodyParser = require('body-parser')
-app.use(bodyParser.json())
-
-app.listen(config.get('api.porta'), ()=>{console.log('API Produtos está funcionando')})
