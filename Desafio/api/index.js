eccc30a5c6d3dc274e0d258f5a49686d9b4ff89c
Bruna Nunes <bruna.nunes@softdesign.com.br>
2021-03-10 09:16:10 -0300

    iniciando projeto, configurações iniciais sequelize-cli e criação do banco _modulo1

---
+++ Desafio/api/index.js
@@ -0,0 +1,17 @@
+const express = require ('express')
+const bodyParser = require ('body-parser')
+
+const app = express()
+
+app.use(bodyParser.json())
+
+const port = 3000
+
+app.get('/teste', (requisicao, resposta) => resposta
+.status(200)
+.send({mensagem: 'Boas vindas ao mundo backend!'})
+)
+
+app.listen(port, () => console.log(`O servidor está rodando na porta ${port}`))
+
+module.exports = app
