1e0d1d970ab09a78a0bafeb3bd3a858cf7ab2f65
Bruna Nunes <bruna.nunes@softdesign.com.br>
2021-03-12 12:20:01 -0300

    refatorando exercicio 2

--- Semana 1/Aula 1 - exercicios/2-exercicios-aula-1.js
+++ Semana 1/Aula 1 - exercicios/2-exercicios-aula-1.js
@@ -11,80 +11,61 @@
 const categoriaDeProdutos = [];
 
 //adicionando itens a lista com status + 5 categorias
-categoriaDeProdutos.push({ id: 1, nome: 'Escritório', status: 'ativo' });
-categoriaDeProdutos.push({ id: 2, nome: 'Limpeza', status: 'inativo' });
-categoriaDeProdutos.push({ id: 3, nome: 'Decoração', status: 'ativo' });
-categoriaDeProdutos.push({ id: 4, nome: 'Eletrônicos', status: 'ativo' });
-categoriaDeProdutos.push({ id: 5, nome: 'Alimentos', status: 'inativo' });
+categoriaDeProdutos.push({ id: 1, nome: "Escritório", status: "ativo" });
+categoriaDeProdutos.push({ id: 2, nome: "Limpeza", status: "inativo" });
+categoriaDeProdutos.push({ id: 3, nome: "Decoração", status: "ativo" });
+categoriaDeProdutos.push({ id: 4, nome: "Eletrônicos", status: "ativo" });
+categoriaDeProdutos.push({ id: 5, nome: "Alimentos", status: "inativo" });
 
 //criando lista vazia de "produtos"
 const produtos = [];
 
 //adicionando produtos
-produtos.push({ id: 1, nome: 'Caneta', preco: 1.50, categoria: 'Escritório' });
-produtos.push({ id: 2, nome: 'Sabão', preco: 4.00, categoria: 'Limpeza' });
-produtos.push({ id: 3, nome: 'Fone de Ouvido', preco: 50.00, categoria: 'Eletrônicos' });
-produtos.push({ id: 4, nome: 'Mouse', preco: 50.00, categoria: 'Eletrônicos' });
+produtos.push({ id: 1, nome: "Caneta", preco: 1.5, categoria: "Escritório" });
+produtos.push({ id: 2, nome: "Sabão", preco: 4.0, categoria: "Limpeza" });
+produtos.push({ id: 3, nome: "Fone de Ouvido", preco: 50.0, categoria: "Eletrônicos",});
+produtos.push({ id: 4, nome: "Mouse", preco: 50.0, categoria: "Eletrônicos" });
 
 //1- Percorrer a lista e mostrar no console só o "nome dos produtos" da categoria "Eletrônicos".
 console.log("----exercicio 1----");
-const procuraProdutoEletronico = produtos.filter(categoria => categoria.categoria === 'Eletrônicos').map(somenteNome => somenteNome.nome);
-console.log(procuraProdutoEletronico);
+const procuraProdutoEletronico = produtos.filter(produto => produto.categoria === "Eletrônicos").map(produto => produto.nome);
 
+console.log(procuraProdutoEletronico);
 
 //2- Percorrer a lista e mostrar no console só o "nome das categorias" com produtos com preço menor que 10
 console.log("----exercicio 2----");
-const procuraPrecoBaixo = produtos.filter(produto => produto.preco < 10).map(somenteNome => somenteNome.nome)
-        console.log(procuraPrecoBaixo);
+const procuraPrecoBaixo = produtos.filter(produto => produto.preco < 10).map(produto => produto.nome);
 
-
+console.log(procuraPrecoBaixo);
 
 //3- Percorrer a lista e mostrar no console só os produtos das categorias desativadas.
 console.log("----exercicio 3----");
-const procuraCategoriaDesativada = categoriaDeProdutos.map(function (categoriaCategoria) {
-    if (categoriaCategoria.status === 'inativo') {
-        catInativa = categoriaCategoria.nome
-        //console.log(catInativa); 
-        let nenhum_inativo = 0;
-        produtos.map(function (produto) {
-            if (produto.categoria === catInativa) {
-                console.log(produto.nome);
-            }
-        });
-    }
+const procuraCategoriaDesativada = categoriaDeProdutos.map(categoria => {
+  if (categoria.status === "inativo") {
+    catInativa = categoria.nome;
+    produtos.forEach(produto => {
+      if (produto.categoria === catInativa) {
+        console.log(produto.nome);
+      }
+    });
+  }
 });
-
 
 
 //4- Percorrer a lista e adicionar na categoria a quantidade de produtos de cada categoria
 console.log("----exercicio 4----");
-const tudoJunto = categoriaDeProdutos.map(categoria => {
-    // find no produto da categoria
-    const prods = produtos.filter(p => p.categoria === categoria.nome);
-
-    // categoria + produto
-    return { ...categoria, produtos: prods };
-    // return {
-    //     id: categoria.id,
-    //     nome: categoria.nome,
-    //     status: categoria.status,
-    //     produtos: prods,
-    // }
+const tudoJunto = categoriaDeProdutos.map((categoria) => {
+    const prods = produtos.filter((p) => p.categoria === categoria.nome); // filter no produto da categoria
+    return { ...categoria, produtos: prods }; // categoria + produto
 });
 
-const numProdutosXCategoria = tudoJunto.map(categoria => ({
-    id: categoria.id,
-    nome: categoria.nome,
-    numProdutos: categoria.produtos.length,
-}))
-
+const numProdutosXCategoria = tudoJunto.map((categoria) => ({
+  id: categoria.id,
+  nome: categoria.nome,
+  numProdutos: categoria.produtos.length,
+}));
 
 console.table(numProdutosXCategoria);
 
 // categoriaDeProdutos.filter
 // categoriaDeProdutos.produtos.filter
-
-
-
-
- 
