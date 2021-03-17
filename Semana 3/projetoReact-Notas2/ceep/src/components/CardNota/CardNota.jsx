4a7e19df8e3be599c6baee06470ec5a3a089b0a3
Bruna Nunes <bruna.nunes@softdesign.com.br>
2021-03-16 22:16:19 -0300

    importando icone como svg/componente

--- Semana 3/projetoReact-Notas2/ceep/src/components/CardNota/CardNota.jsx
+++ Semana 3/projetoReact-Notas2/ceep/src/components/CardNota/CardNota.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import "./estilo.css";
+// import deleteSVG from "../../assets/img/delete.svg" //importar como imagem
+import {ReactComponent as DeleteSVG} from "../../assets/img/delete.svg" //como componente
 
 class CardNota extends Component {
   render() {
@@ -7,6 +9,9 @@ class CardNota extends Component {
       <section className="card-nota">
         <header className="card-nota_cabecalho">
           <h3 className="card-nota_titulo">{this.props.titulo}</h3>
+          {/* <img src={deleteSVG}></img> */}
+          <DeleteSVG></DeleteSVG>
+
         </header>
         <p className="card-nota_texto">{this.props.texto}</p>
       </section>
