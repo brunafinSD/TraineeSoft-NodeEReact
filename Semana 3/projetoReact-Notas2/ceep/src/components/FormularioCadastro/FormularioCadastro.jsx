463131c1174a998bc63c6e2670114b23e2984bce
Bruna Nunes <bruna.nunes@softdesign.com.br>
2021-03-17 10:08:39 -0300

    adicionando método desinscrever e componentWillUnmount()

--- Semana 3/projetoReact-Notas2/ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ Semana 3/projetoReact-Notas2/ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -7,11 +7,16 @@ class FormularioCadastro extends Component {
     this.titulo = "";
     this.texto = "";
     this.categoria = "Sem categoria";
-    this.state = {categorias:[]}
+    this.state = { categorias: [] };
+    this._novasCategorias = this._novasCategorias.bind(this);
   }
 
   componentDidMount() {
-    this.props.categorias.inscrever(this._novasCategorias.bind(this));
+    this.props.categorias.inscrever(this._novasCategorias);
+  }
+
+  componentWillUnmount() {
+    this.props.categorias.desinscrever(this._novasCategorias);
   }
 
   _novasCategorias(categorias) {
--- Semana 3/projetoReact-Notas2/ceep/src/components/ListaDeCategorias/ListaDeCategorias.jsx
+++ Semana 3/projetoReact-Notas2/ceep/src/components/ListaDeCategorias/ListaDeCategorias.jsx
@@ -2,14 +2,18 @@ import React, { Component } from "react";
 import "./estilo.css";
 
 class ListaDeCategorias extends Component {
-  constructor(){
+  constructor() {
     super();
-    this.state={categorias:[]}
-
+    this.state = { categorias: [] };
+    this._novasCategorias = this._novasCategorias.bind(this);
   }
 
   componentDidMount() {
-    this.props.categorias.inscrever(this._novasCategorias.bind(this));
+    this.props.categorias.inscrever(this._novasCategorias);
+  }
+
+  componentWillUnmount() {
+    this.props.categorias.desinscrever(this._novasCategorias);
   }
 
   _novasCategorias(categorias) {
