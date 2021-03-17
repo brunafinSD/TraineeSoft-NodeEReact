1bed7862d9b9d4afc5edaab4f3cf2385eab7e746
Bruna Nunes <bruna.nunes@softdesign.com.br>
2021-03-17 13:30:17 -0300

    refatorando e corrindo exercícios

--- Semana 1/Exercicios Promises/exercicioPromisesAula1.js
+++ Semana 1/Exercicios Promises/exercicioPromisesAula1.js
@@ -1,5 +1,5 @@
-//5 - Corrigir as duas últimas linhas para mostrar a lista corretamente usando callback
-///////////////////////
+// //5 - Corrigir as duas últimas linhas para mostrar a lista corretamente usando callback
+// ///////////////////////
 var list = [];
 const databaseInsert = function (data, callback) {
   setTimeout(function () {
@@ -9,41 +9,49 @@ const databaseInsert = function (data, callback) {
     }
   }, 0);
 };
-///////////////////////
+
 databaseInsert({ meuobjeto: "a" }, () => {
-  console.log("Com Callback / lista:\n ", list);
+  console.log("\nCom Callback / lista:\n ", list);
 });
+///////////////////////
 
 //6 - Refatorar para promise
 
 var list2 = [];
-const databaseInsert2 = function (data, callback) {
+const databaseInsert2 = function (data) {
   const promise = new Promise((resolve, reject) => {
-    setTimeout(function () {
-      list2.push(data);
-      if (callback) {
-        resolve(callback());
-      }
-    }, 0);
+    list2.push(data);
+    resolve(list2);
   });
   return promise;
 };
-///////////////////////
-databaseInsert2({ meuobjeto: "a" }, () => {
-  console.log("\nCom Promises / lista: \n ", list2);
-}).then();
 
-//7 - Refatorar para async / await
-var list3 = [];
-const databaseInsert3 = async function (data, callback) {
-  await setTimeout(function () {
-    list3.push(data);
-    if (callback) {
-      callback();
-    }
-  }, 0);
-};
+databaseInsert2({ meuobjeto: "a" }).then((resultado) =>
+  console.log("\nCom Promise / lista:\n ", resultado)
+);
 ///////////////////////
-databaseInsert3({ meuobjeto: "a" }, () => {
-  console.log("\nCom Async Await / lista: \n ", list3);
-}).then();
+
+// //7 - Refatorar para async / await
+var list3 = [];
+
+
+const databaseInsert3 = function (data) {
+  const promise = new Promise((resolve, reject) => {
+    list3.push(data);
+    resolve(list3);
+  });
+  return promise;
+};
+
+async function main(data){
+  const valor = await databaseInsert3(data);
+  console.log("\nCom Async / lista:\n ", valor)
+}
+main({ meuobjeto: "a" })
+
+
+// databaseInsert3({ meuobjeto: "a" }).then((resultado) =>
+//   console.log("\nCom Async / lista:\n ", resultado)
+// );
+
+///////////////////////
