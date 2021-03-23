0788831eb93bbfe3f526e7c80c7eef0e30bdfc55
Bruna Nunes <bruna.nunes@softdesign.com.br>
2021-03-23 12:43:16 -0300

    Revert "comitando para ajuda (erro quando tiro método criar categoria do app.js)"
    This reverts commit c8a9471b1926ad696e133e652d4914a06583a63b.

--- Semana 3/desafio/listarefas/src/App.js
+++ Semana 3/desafio/listarefas/src/App.js
@@ -2,25 +2,24 @@ import React, { Component } from "react";
 import ListaDeTarefas from "./components/ListaDeTarefas/";
 import FormularioDeCadastro from "./components/FormularioDeCadastro/";
 import "./assets/index.css";
-import ArrayTarefas from "./dados/Tarefas";
 
 class App extends Component {
   constructor() {
     super();
-    this.tarefas = new ArrayTarefas();
-    // this.state = {
-    //   tarefas: [],
-    // };
+
+    this.state = {
+      tarefas: [],
+    };
   }
 
-  // criarTarefa(descricao, status, id) {
-  //   const novaTarefa = { descricao, status, id };
-  //   const novoArrayTarefas = [...this.state.tarefas, novaTarefa];
-  //   const novoEstado = {
-  //     tarefas: novoArrayTarefas,
-  //   };
-  //   this.setState(novoEstado);
-  // }
+  criarTarefa(descricao, status, id) {
+    const novaTarefa = { descricao, status, id };
+    const novoArrayTarefas = [...this.state.tarefas, novaTarefa];
+    const novoEstado = {
+      tarefas: novoArrayTarefas,
+    };
+    this.setState(novoEstado);
+  }
 
   atualizarStatus(id) {
     const tarefaAtualizada = this.state.tarefas.map((tarefa) => {
@@ -33,23 +32,22 @@ class App extends Component {
 
   //função extra
   deletarTarefa(id) {
-    const arrayTarefaDeletada = this.state.tarefas;
-    var indice = arrayTarefaDeletada.findIndex((tarefa) => tarefa.id === id); //findIndex pega o index do array
-    arrayTarefaDeletada.splice(indice, 1);
-    this.setState(arrayTarefaDeletada);
+    const arrayTarefaDeletada = this.state.tarefas
+    var indice = arrayTarefaDeletada.findIndex(tarefa=>tarefa.id === id) //findIndex pega o index do array
+    arrayTarefaDeletada.splice(indice, 1)
+    this.setState(arrayTarefaDeletada)
   }
 
   render() {
     return (
       <section className="form-cadastro">
         <FormularioDeCadastro
-          criarTarefa={this.tarefas.criarTarefa}
+          criarTarefa={this.criarTarefa.bind(this)}
         ></FormularioDeCadastro>
         <ListaDeTarefas
           deletarTarefa={this.deletarTarefa.bind(this)}
           atualizarStatus={this.atualizarStatus.bind(this)}
-          //tarefas={this.state.tarefas}
-          tarefas={this.tarefas.tarefas}
+          tarefas={this.state.tarefas}
         ></ListaDeTarefas>
       </section>
     );
