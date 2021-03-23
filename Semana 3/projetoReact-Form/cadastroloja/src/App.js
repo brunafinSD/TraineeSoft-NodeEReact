8ed040d6f0d310fb5cf0b5abcd41135ae479bc26
Bruna Nunes <bruna.nunes@softdesign.com.br>
2021-03-23 17:15:00 -0300

    criando component Formulario

--- Semana 3/projetoReact-Form/cadastroloja/src/App.js
+++ Semana 3/projetoReact-Form/cadastroloja/src/App.js
@@ -1,7 +1,15 @@
+import { Component, Fragment } from "react";
 import "./App.css";
+import FormularioCadastro from "./components/FormularioCadastro/FormularioCadastro";
 
-function App() {
-  return <p>Formulário de cadastro</p>;
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <p>Formulário de cadastro</p>
+        <FormularioCadastro></FormularioCadastro>
+      </Fragment>
+    );
+  }
 }
-
 export default App;
